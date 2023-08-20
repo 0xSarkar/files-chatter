c94374f7c939bd6d6b81b12950448c8b27457d0a
0xSarkar <0xsarkar@gmail.com>
2023-08-20 14:00:19 +0530

    removed separate scrollbar for chatbox. Now using scrolledtext widget for conversation and chatbox widgets. Also improved lineheight of txt_conv

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import scrolledtext
 
-### === Widget Event Handler Functions === ###
+### === Widget Event Handlers === ###
 
 def send_btn_click():
     print("Button clicked!")
 
+def chatbox_select_all(event):
+    txt_chatbox.tag_add(tk.SEL, "1.0", tk.END)
+    return "break"
 
 ### === Root Window === ###
 
@@ -33,11 +37,13 @@ style.theme_use("clam")
 ## === Widgets === ###
 
 # Conversation Text
-txt_conv = tk.Text(
+txt_conv = tk.scrolledtext.ScrolledText(
     root,
     height=5,
     width=52,
     wrap=tk.WORD,
+    spacing1=10,
+    spacing2=4,
     highlightthickness=0,  # Remove the focus border
     bd=0,  # Remove the border
     relief=tk.FLAT,  # Set the relief to flat
@@ -45,29 +51,30 @@ txt_conv = tk.Text(
     pady=6,
 )
  
-Fact = """A man can be arrested in
-Italy for wearing a skirt in public."""
+Fact = """A long long very very long long very verylong long very very long long very very sentence.
+A man can be arrested in Italy for wearing a skirt in public."""
 
-txt_conv.grid(row=0, column=0, columnspan=3, sticky="nsew")
+txt_conv.grid(row=0, column=0, columnspan=2, sticky="nsew")
 
 # Insert The Fact.
 txt_conv.insert(tk.END, Fact)
 
-txt_conv.config(state=tk.DISABLED, selectbackground="lightblue", inactiveselectbackground="lightblue")
+txt_conv.config(
+    state=tk.DISABLED,
+    selectbackground="lightblue", 
+    inactiveselectbackground="lightblue",
+)
 
 # Chat box
-txt_chatbox = tk.Text(root, height=3, width=1)
+txt_chatbox = tk.scrolledtext.ScrolledText(root, height=3, width=1)
 txt_chatbox.grid(row=1, column=0, padx=(8, 2), pady=8, sticky="nsew")
 
-# Add a Scrollbar(vertical) for Text Area
-text_area_scrollbar= ttk.Scrollbar(root, orient='vertical', command=txt_chatbox.yview)
-text_area_scrollbar.grid(row=1, column=1, padx=(0, 2), pady=8, sticky="nsew")
-
-txt_chatbox['yscrollcommand'] = text_area_scrollbar.set
+# Bind Ctrl+A to select all text
+txt_chatbox.bind("<Control-a>", chatbox_select_all)
 
 # Send Button
 btn_send = ttk.Button(root, text="Send", command=send_btn_click)
-btn_send.grid(row=1, column=2, padx=(2, 8), pady=8, sticky="nsew")
+btn_send.grid(row=1, column=1, padx=(2, 8), pady=8, sticky="nsew")
 
 
 ### === Grid Configuration === ###
@@ -75,7 +82,6 @@ btn_send.grid(row=1, column=2, padx=(2, 8), pady=8, sticky="nsew")
 # Set column weights to make them resize proportionally
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=0)
-root.grid_columnconfigure(2, weight=0)
 
 # Configure row 0 to expand vertically
 root.grid_rowconfigure(0, weight=1)
